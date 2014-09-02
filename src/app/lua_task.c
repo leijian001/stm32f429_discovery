0690956221659731a5ccafdd8fd4de4901e49223
electron <937431539@qq.com>
2014-09-02 12:45:57 +0800

    add luatask.c

---
+++ src/app/lua_task.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <raw_api.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+/******************************************************************************/
+#define LUA_TASK_STK_SIZE 		(16 *1024)
+static PORT_STACK 				lua_task_stk[LUA_TASK_STK_SIZE];
+static RAW_TASK_OBJ 			lua_task_obj;
+/******************************************************************************/
+
+static void lua_task(void *pdat)
+{
+	(void)pdat;
+	
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	
+	for(;;)
+	{
+		raw_task_suspend(raw_task_identify());
+	}
+}
+
+void lua_task_init(unsigned char prio)
+{
+	raw_task_create(&lua_task_obj,			/* 任务控制块地址 	*/
+					(RAW_U8  *)"lua_task",	/* 任务名 			*/
+					(void *)0,				/* 任务参数 		*/
+					prio,					/* 优先级 			*/
+					0,						/* 时间片 			*/
+					lua_task_stk,			/* 任务栈首地址 	*/
+					LUA_TASK_STK_SIZE ,		/* 任务栈大小 		*/
+					lua_task,				/* 任务入口地址 	*/
+					1);						/* 是否立即运行 	*/
+}
