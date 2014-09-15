2311e8a8ad176be8e5c1feb88c07d5a1adb2240b
electron <937431539@qq.com>
2014-09-15 23:38:56 +0800

    修改了任务栈大小，提高了内存利用率

--- src/app/sys_led.c
+++ src/app/sys_led.c
@@ -3,7 +3,7 @@
 #include <raw_api.h>
 
 /******************************************************************************/
-#define SYS_LED_TASK_STK_SIZE 	(768)
+#define SYS_LED_TASK_STK_SIZE 	(512)
 static PORT_STACK 				sys_led_task_stk[SYS_LED_TASK_STK_SIZE];
 static RAW_TASK_OBJ 			sys_led_task_obj;
 /******************************************************************************/
